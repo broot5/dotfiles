56a3faf8e17ac4962d70ab6d3ca0c3a1cfda8251
broot5 <broot5dot@gmail.com>
2020-10-18 22:39:30 +0900

    update

---
+++ wallpaper/get_unspla.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -G -f -v -L -# http://source.unsplash.com/random/3840x2160 -o /tmp/wallpaper.png
+
+convert /tmp/wallpaper.png -filter Gaussian -blur 0x16 /tmp/wallpaper.png
+
+swaymsg 'output "*" background /tmp/wallpaper.png fill'
